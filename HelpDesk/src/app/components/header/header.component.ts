34e96d0fa10c3d91207181be8b0c434501b6567d
unknown <alvesmlktop@gmail.com>
2023-12-14 11:46:33 -0300

    html e css para header

--- HelpDesk/src/app/components/header/header.component.ts
+++ HelpDesk/src/app/components/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(){}
 
+  ngOnInit(): void {
+    
+  }
 }
