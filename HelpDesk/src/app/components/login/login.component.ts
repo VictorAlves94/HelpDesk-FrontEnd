ad62451f22259874fa83a7fd0503853b55f92bb9
unknown <alvesmlktop@gmail.com>
2023-12-14 14:40:16 -0300

    validacao de formularios para login

--- HelpDesk/src/app/components/login/login.component.ts
+++ HelpDesk/src/app/components/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Credenciais } from '../../Models/credenciais';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  creds: Credenciais = {
+    email:'',
+    senha:''
+  }
+  email = new FormControl(null,Validators.email);
+  senha = new FormControl(null,Validators.minLength(3));
+  constructor(){}
+  ngOnInit(): void {
+
+    
+  }
+  validaCampos():boolean{
+    if(this.email.valid && this.senha.valid){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
 
 }
