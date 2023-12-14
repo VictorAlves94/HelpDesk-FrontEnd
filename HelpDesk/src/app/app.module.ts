20fd901cac07f0f58b1ec5daf1b9cdf40e7a87d5
unknown <alvesmlktop@gmail.com>
2023-12-14 13:32:26 -0300

    component login criado

--- HelpDesk/src/app/app.module.ts
+++ HelpDesk/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -36,7 +37,8 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
     NavComponent,
     HomeComponent,
     HeaderComponent,
-    TecnicoListComponent
+    TecnicoListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
