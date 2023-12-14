d16b989871283809ebeb6986140d3fbc2fe7feb4
unknown <alvesmlktop@gmail.com>
2023-12-14 09:29:13 -0300

    rota para componente nav

--- HelpDesk/src/app/app-routing.module.ts
+++ HelpDesk/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: NavComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- HelpDesk/src/app/app.module.ts
+++ HelpDesk/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+//componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
 
 
