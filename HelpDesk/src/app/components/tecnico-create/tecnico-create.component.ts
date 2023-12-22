b4b5202c20f1c9f1dde2a7026747153be32fcd7f
unknown <alvesmlktop@gmail.com>
2023-12-22 17:37:00 -0300

    componente create tecnico finalizado

--- HelpDesk/src/app/components/tecnico-create/tecnico-create.component.ts
+++ HelpDesk/src/app/components/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { TecnicoService } from '../../services/tecnico.service';
+import { Tecnico } from '../../Models/tecnico';
+import { Toast, ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,21 +12,56 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class TecnicoCreateComponent implements OnInit{
 
+  tecnico: Tecnico ={
+    id: '',
+    nome: '',
+    cpf: '',
+    email:'',
+    senha: '',
+    perfis:[],
+    dataCriacao:''
+  }
+
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor(){
+  constructor(
+    private service:TecnicoService,
+    private toast: ToastrService,
+    private router:Router
+  ){
 
   }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+
+  create():void {
+    this.service.create(this.tecnico).subscribe(()=>{
+      this.toast.success('Técnico cadastrado com sucesso!', 'Cadastro');
+      this.router.navigate(['tecnicos'])
+    },ex => {
+    if(ex.error.errors){
+      ex.error.errors.forEach(element => {
+      this.toast.error(element.message);
+    });
+    } else{
+      this.toast.error(ex.error.message);
+    } 
+  })
   }
 
   validaCampos(): boolean{
     return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
   }
 
+  addPerfil(perfil: any):void{
+    if(this.tecnico.perfis.includes(perfil)){
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil),1);
+      console.log(this.tecnico.perfis);
+    }else{this.tecnico.perfis.push(perfil);
+      console.log(this.tecnico.perfis);}
+  }
 }
--- HelpDesk/src/app/services/tecnico.service.ts
+++ HelpDesk/src/app/services/tecnico.service.ts
@@ -14,4 +14,9 @@ export class TecnicoService {
     return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
   }
 
+  create(tecnico: Tecnico):Observable<Tecnico>{
+    return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
+    
+  }
+
 }
