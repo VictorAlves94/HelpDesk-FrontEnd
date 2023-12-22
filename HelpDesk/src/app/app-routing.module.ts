f05b4c7ff16aa836d75ed2c2e28e4fe27b1db088
unknown <alvesmlktop@gmail.com>
2023-12-22 13:11:48 -0300

    rota para componente tecnico para cliente

--- HelpDesk/src/app/app-routing.module.ts
+++ HelpDesk/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TecnicoCreateComponent } from './components/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   {path: 'login', component:LoginComponent},
@@ -12,7 +13,9 @@ const routes: Routes = [
   
     path: '',component: NavComponent, canActivate: [AuthGuard],children:[
       {path:'home', component: HomeComponent},
-      {path:'tecnicos', component:TecnicoListComponent}
+
+      {path:'tecnicos', component:TecnicoListComponent},
+      {path:'tecnicos/create', component:TecnicoCreateComponent}
     ]
   }
   
