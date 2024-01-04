80874df3ca9183aff0038b0f98f97b736cdc1017
unknown <alvesmlktop@gmail.com>
2024-01-04 13:50:37 -0300

    tecnico delete finalizado

--- HelpDesk/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ HelpDesk/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { TecnicoService } from '../../../services/tecnico.service';
+import { Tecnico } from '../../../Models/tecnico';
+import { Toast, ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tecnico-delete',
@@ -6,5 +11,51 @@ import { Component } from '@angular/core';
   styleUrl: './tecnico-delete.component.css'
 })
 export class TecnicoDeleteComponent {
+
+  tecnico: Tecnico ={
+    id: '',
+    nome: '',
+    cpf: '',
+    email:'',
+    senha: '',
+    perfils:[],
+    dataCriacao:''
+  }
+  constructor(
+    private service:TecnicoService,
+    private toast: ToastrService,
+    private router:Router,
+    private route: ActivatedRoute
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+    
+  }
+  findById():void{
+    this.service.findById(this.tecnico.id).subscribe(resposta => {
+      resposta.perfils = [];
+      this.tecnico = resposta;
+    })
+  }
+
+  delete():void {
+    this.service.delete(this.tecnico.id).subscribe(()=>{
+      this.toast.success('Técnico deletado com sucesso!', 'Deletar');
+      this.router.navigate(['tecnicos'])
+    },ex => {
+    if(ex.error.errors){
+      ex.error.errors.forEach(element => {
+      this.toast.error(element.message);
+    });
+    } else{
+      this.toast.error(ex.error.message);
+    } 
+  })
+  }
+
 
 }
--- HelpDesk/src/app/services/tecnico.service.ts
+++ HelpDesk/src/app/services/tecnico.service.ts
@@ -27,5 +27,10 @@ export class TecnicoService {
     return this.http.put<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${tecnico.id}`,tecnico);
 
   }
+  delete(id:any):Observable<Tecnico>{
+    return this.http.delete<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${id}`)
+
+  }
+
 
 }
