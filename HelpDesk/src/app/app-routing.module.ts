c45b8bf0b19ab7cc70e6527401e98dfcb306a9d1
unknown <alvesmlktop@gmail.com>
2024-01-04 14:39:30 -0300

    rota para chamado list criado

--- HelpDesk/src/app/app-routing.module.ts
+++ HelpDesk/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 const routes: Routes = [
   {path: 'login', component:LoginComponent},
@@ -29,7 +30,10 @@ const routes: Routes = [
       {path:'clientes', component:ClienteListComponent},
       {path:'clientes/create', component:ClienteCreateComponent},
       {path:'clientes/update/:id', component:ClienteUpdateComponent},
-      {path:'clientes/delete/:id', component:ClienteDeleteComponent}
+      {path:'clientes/delete/:id', component:ClienteDeleteComponent},
+
+
+      {path:'chamados', component:ChamadoListComponent},
     
     
     ]
